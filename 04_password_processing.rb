28d56c540c162ba2ffc654b12983a971acc70fe8
adventcoder <59347361+adventcoder@users.noreply.github.com>
2020-12-06 01:27:30 +0000

    Clean up day 4 a bit

--- 04_password_processing.rb
+++ 04_password_processing.rb
@@ -3,37 +3,30 @@ require_relative '00_common.rb'
 
 input = get_input(4)
 
-def is_valid?(port, strict = false)
-  return false unless ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'] - port.keys == []
-  if strict
-    return false unless port['byr'] =~ /^([0-9]{4})$/ and $1.to_i.between?(1920, 2002)
-    return false unless port['iyr'] =~ /^([0-9]{4})$/ and $1.to_i.between?(2010, 2020)
-    return false unless port['eyr'] =~ /^([0-9]{4})$/ and $1.to_i.between?(2020, 2030)
-    if port['hgt'] =~ /^(\d+)cm$/
-      return false unless $1.to_i.between?(150, 193)
-    elsif port['hgt'] =~ /^(\d+)in$/
-      return false unless $1.to_i.between?(59, 76)
-    else
-      return false
-    end
-    return false unless port['hcl'] =~ /^#[0-9a-f]{6}$/
-    return false unless ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].include?(port['ecl'])
-    return false unless port['pid'] =~ /^[0-9]{9}$/
-  end
-  return true
-end
+validators = {}
+validators['byr'] = lambda { |v| v =~ /^([0-9]{4})$/ && $1.to_i.between?(1920, 2002) }
+validators['iyr'] = lambda { |v| v =~ /^([0-9]{4})$/ && $1.to_i.between?(2010, 2020) }
+validators['eyr'] = lambda { |v| v =~ /^([0-9]{4})$/ && $1.to_i.between?(2020, 2030) }
+validators['hgt'] = lambda { |v| (v =~ /^([0-9]+)cm$/ && $1.to_i.between?(150, 193)) || (v =~ /^([0-9]+)in$/ && $1.to_i.between?(59, 76)) }
+validators['hcl'] = lambda { |v| v =~ /^#[0-9a-f]{6}$/ }
+validators['ecl'] = lambda { |v| ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].include?(v) }
+validators['pid'] = lambda { |v| v =~ /^[0-9]{9}$/ }
 
 count1 = 0
 count2 = 0
-curr = {}
+passport = {}
 input.each_line do |line|
   if line.strip.empty?
-    count1 += 1 if is_valid?(curr, false)
-    count2 += 1 if is_valid?(curr, true)
-    curr = {}
+    if validators.keys.all? { |k| passport.has_key?(k) }
+      count1 += 1
+      if validators.all? { |k, v| v.call(passport[k]) }
+        count2 += 1
+      end
+    end
+    passport = {}
   else
-    for entry in line.strip.split
-      curr[entry.split(':')[0]] = entry.split(':')[1]
+    for token in line.split
+      passport.store(*token.split(':'))
     end
   end
 end
