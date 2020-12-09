0e205b20190a5831de7e9a7849ae2875b28b139f
adventcoder <59347361+adventcoder@users.noreply.github.com>
2020-12-09 05:50:37 +0000

    Better day 9

--- 09_encoding_error.rb
+++ 09_encoding_error.rb
@@ -8,29 +8,26 @@ input.each_line do |line|
   nums << line.chomp.to_i
 end
 
-def valid?(n, ns)
-  ns.combination(2) do |a, b|
-    if a + b == n
-      return true
-    end
-  end
-  false
-end
-
 for i in 25 ... nums.size
-  if !valid?(nums[i], nums[i - 25 ... i])
-    x = nums[i]
+  if nums[i - 25 ... i].combination(2).none? { |a, b| a + b == nums[i] }
+    invalid = nums[i]
     break
   end
 end
+puts invalid
 
-puts x
-
-
-for l in 2 ... 50
-  nums.each_cons(l) do |as|
-    if as.inject(0, &:+) == x
-      puts as.min + as.max
-    end
+first = 0
+last = 0
+sum = nums[0]
+until sum == invalid
+  if sum < invalid
+    last += 1
+    sum += nums[last]
+  else
+    sum -= nums[first]
+    first += 1
   end
 end
+range = nums[first .. last]
+weakness = range.min + range.max
+puts weakness
