219660fcebfe232ccee26b389dfefdcf766e583f
adventcoder <59347361+adventcoder@users.noreply.github.com>
2020-12-11 07:33:57 +0000

    Clean up some more

--- 11_seating_system.rb
+++ 11_seating_system.rb
@@ -1,78 +1,56 @@
 require_relative '00_common.rb'
 
+DIR8 = [*-1 .. 1].product([*-1 .. 1]).select { |(x, y)| [x.abs, y.abs].max == 1 }
+
+def valid?(grid, x, y)
+  y.between?(0, grid.size - 1) && x.between?(0, grid[y].size - 1)
+end
+
+def occupied_adjacent(grid, x, y)
+  count = 0
+  for dx, dy in DIR8
+    x1 = x + dx
+    y1 = y + dy
+    count += 1 if valid?(grid, x1, y1) && grid[y1][x1] == '#'
+  end
+  count
+end
+
+def occupied_visible(grid, x, y)
+  count = 0
+  for dx, dy in DIR8
+    x1 = x + dx
+    y1 = y + dy
+    while valid?(grid, x1, y1) && grid[y1][x1] == '.'
+      x1 += dx
+      y1 += dy
+    end
+    count += 1 if valid?(grid, x1, y1) && grid[y1][x1] == '#'
+  end
+  count
+end
+
+def simulate(input, occupied, max_occupied)
+  grid = input.lines.map(&:chomp)
+  loop do
+    copy = grid.map(&:dup)
+    changed = false
+    for y in 0 ... grid.size
+      for x in 0 ... grid[y].size
+        if grid[y][x] == 'L' && occupied.call(grid, x, y) == 0
+          copy[y][x] = '#'
+          changed = true
+        elsif grid[y][x] == '#' && occupied.call(grid, x, y) >= max_occupied
+          copy[y][x] = 'L'
+          changed = true
+        end
+      end
+    end
+    return grid if !changed
+    grid = copy
+  end
+end
+
 input = get_input(11)
-
-def valid?(rows, x, y)
-  y >= 0 and y < rows.size and x >= 0 and x < rows[y].size
-end
-
-def count(rows)
-  rows.sum { |row| row.count('#') }
-end
-
-def count_adjacent(rows, x, y)
-  n = 0
-  for dy in -1 .. 1
-    for dx in -1 .. 1
-      next if dx == 0 and dy == 0
-      x1 = x + dx
-      y1 = y + dy
-      n += 1 if valid?(rows, x1, y1) and rows[y1][x1] == '#'
-    end
-  end
-  n
-end
-
-def count_visible(rows, x, y)
-  n = 0
-  for dy in -1 .. 1
-    for dx in -1 .. 1
-      next if dx == 0 and dy == 0
-      x1 = x + dx
-      y1 = y + dy
-      while valid?(rows, x1, y1) and rows[y1][x1] == '.'
-        x1 += dx
-        y1 += dy
-      end
-      n += 1 if valid?(rows, x1, y1) and rows[y1][x1] == '#'
-    end
-  end
-  n
-end
-
-curr = input.lines.map { |line| line.chomp }
-loop do
-  prev = curr.map { |row| row.dup }
-  for y in 0 ... curr.size
-    for x in 0 ... curr[y].size
-      if curr[y][x] == 'L' and count_adjacent(prev, x, y) == 0
-        curr[y][x] = '#'
-      elsif curr[y][x] == '#' and count_adjacent(prev, x, y) >= 4
-        curr[y][x] = 'L'
-      end
-    end
-  end
-  if curr == prev
-    puts count(curr)
-    break
-  end
-end
-
-
-curr = input.lines.map { |line| line.chomp }
-loop do
-  prev = curr.map { |row| row.dup }
-  for y in 0 ... curr.size
-    for x in 0 ... curr[y].size
-      if curr[y][x] == 'L' and count_visible(prev, x, y) == 0
-        curr[y][x] = '#'
-      elsif curr[y][x] == '#' and count_visible(prev, x, y) >= 5
-        curr[y][x] = 'L'
-      end
-    end
-  end
-  if curr == prev
-    puts count(curr)
-    break
-  end
-end
+puts simulate(input, method(:occupied_adjacent), 4).sum { |row| row.count('#') }
+puts simulate(input, method(:occupied_visible), 5).sum { |row| row.count('#') }
