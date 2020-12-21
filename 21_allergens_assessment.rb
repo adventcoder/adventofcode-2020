b8d7a67e189fd0336728d4d032a49a7fb66ce8ed
adventcoder <59347361+adventcoder@users.noreply.github.com>
2020-12-21 07:55:08 +0000

    Cleaned up

--- 21_allergens_assessment.rb
+++ 21_allergens_assessment.rb
@@ -2,49 +2,37 @@ require_relative 'common'
 
 input = get_input(21)
 
-allergen_counts = Hash.new { |h, k| h[k] = Hash.new(0) }
-min_count = Hash.new { |h, k| h[k] = 0 }
-all_foods = []
-food_counts = Hash.new { |h, k| h[k] = 0 }
+allergen_candidates = {}
+ingredient_count = Hash.new(0)
 
 input.each_line do |line|
-  i1 = line.index('(')
-  i2 = line.index(')', i1 + 1)
-  x = line[(i1 + 1) ... i2]
-  allergens = line[(i1 + 1 + 'contains'.size) ... i2].split(',').map(&:strip)
-  foods = line[0 ... i1].split
-  foods.each do |f|
-    food_counts[f] += 1
+  allergens = []
+  if line.slice!(/\(contains (.*)\)/)
+    allergens = $1.split(', ')
   end
-  all_foods |= foods
-  allergens.each do |a|
-    min_count[a] += 1
-    foods.each do |f|
-      allergen_counts[a][f] += 1
+  ingredients = line.split
+
+  ingredients.each do |food|
+    ingredient_count[food] += 1
+  end
+
+  allergens.each do |allergen|
+    if allergen_candidates[allergen] == nil
+      allergen_candidates[allergen] = ingredients
+    else
+      allergen_candidates[allergen] &= ingredients
     end
   end
 end
 
-possible = Hash.new { |h, k| h[k] = [] }
-all_possible = []
-allergen_counts.each do |a, fs|
-  fs.each do |f, c|
-    if c >= min_count[a]
-      possible[a] << f
-    end
-  end
-  all_possible |= possible[a]
+puts (ingredient_count.keys - allergen_candidates.values.flatten).sum { |k| ingredient_count[k] }
+
+allergens = {}
+until allergen_candidates.empty?
+  allergen = allergen_candidates.keys.find { |k| allergen_candidates[k].size == 1 }
+  ingredient = allergen_candidates.delete(allergen).first
+  allergens[ingredient] = allergen
+  allergen_candidates.each_value { |ingredients| ingredients.delete(ingredient) }
 end
 
-puts (all_foods - all_possible).sum { |f| food_counts[f] }
-
-actual = {}
-until possible.empty?
-  a = possible.keys.find { |a| possible[a].size == 1 }
-  f = possible[a][0]
-  actual[f] = a
-  possible.delete(a)
-  possible.each_value { |fs| fs.delete(f) }
-end
-
-puts actual.keys.sort_by { |f| actual[f] }.join(',')
+puts allergens.keys.sort_by { |ingredient| allergens[ingredient] }.join(',')
