c9a98a60945d54db20a80512580f41e64f944233
adventcoder <59347361+adventcoder@users.noreply.github.com>
2020-12-22 12:13:06 +0000

    Was meaning to come back to this one

--- 10_adapter_array.rb
+++ 10_adapter_array.rb
@@ -3,44 +3,56 @@ require_relative 'common.rb'
 
 input = get_input(10)
 
-jolts = []
-input.each_line do |line|
-  jolts << line.chomp.to_i
-end
+joltages = input.lines.map(&:to_i)
+joltages.sort!
+joltages.unshift(0)
+joltages.push(joltages.last + 3)
 
-jolts.sort!
-jolts.unshift(0)
-jolts << jolts.last + 3
+diffs = joltages.each_cons(2).map { |a, b| b - a }
 
-diffs = Hash.new(0)
-jolts.each_cons(2) { |a, b| diffs[b - a] += 1 }
+puts diffs.count(1) * diffs.count(3)
 
-puts diffs[3] * diffs[1]
+#
+# For any run of adapters only one joltage apart, we can remove a single adapter and still get a valid sequence:
+#
+# For example: 1,2,3,...,n and 1,3,...,n are both valid.
+#
+# There are n-2 ways to do this, for n >= 2.
+# (We assume there is a gap of 3 on either side, so can't remove the first or last adapter.)
+#
+# We can also remove pairs of adapters from the run.
+#
+# For example: 1,2,3,4,...,n -> 1,4,...,n
+# Or non-consecutive: 1,2,3,4,5,...,n -> 1,3,5,...,n
+#
+# There are n-2C2 ways to do this for n >= 2.
+#
+# It's not possible to remove three or more adapters and still get a valid sequence.
+#
+# This gives: n-2C0 + n-2C1 + n-2C2, sequences for n adapters all 1 joltage apart.
+# Or equivalently: n-1C0 + n-1C1 + n-1C2 = 1 + nC2, sequences for n differences all equal to 1.
+#
 
-def combs_recur(jolts, start = 0, memo = {})
-  memo[start] ||= begin
-    n = 0
-    if start == jolts.size - 1
-      n += 1
-    else
-      # add up the combinations for each possible next adapter
-      for i in start + 1 ... jolts.size
-        break unless jolts[i] - jolts[start] <= 3
-        n += combs_recur(jolts, i, memo)
-      end
-    end
-    n
+def run_sequences(diff, n)
+  case diff
+  when 1
+    1 + (n >= 1 ? n*(n-1)/2 : 0)
+  when 3
+    1
   end
 end
 
-# Not my solution but of course the number of ways to end at an adapter is the number of ways for all the possible previous adapters added up.
-def combs_dp(jolts)
-  combs = Hash.new(0)
-  combs[jolts.first] = 1
-  for jolt in jolts.drop(1)
-    combs[jolt] = combs[jolt - 1] + combs[jolt - 2] + combs[jolt - 3]
+def all_sequences(diffs)
+  count = 1
+  i = 0
+  while i < diffs.size
+    start = i
+    begin
+      i += 1
+    end while diffs[i] == diffs[start]
+    count *= run_sequences(diffs[start], i - start)
   end
-  combs[jolts.last]
+  count
 end
 
-puts combs_recur(jolts)
+puts all_sequences(diffs)
