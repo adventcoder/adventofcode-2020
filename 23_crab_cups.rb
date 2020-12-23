532a78bbc186d3e9fad6091796fb7fa0f06ff277
adventcoder <59347361+adventcoder@users.noreply.github.com>
2020-12-23 13:24:17 +0000

    Improved

--- 23_crab_cups.rb
+++ 23_crab_cups.rb
@@ -1,121 +1,51 @@
 require_relative 'common'
 
-
 class Cups
-  class Node
-    include Enumerable 
+  attr_reader :size
 
-    attr_accessor :cup, :next
-
-    def initialize(cup, _next = nil)
-      @cup = cup
-      @next = _next
+  def initialize(cups, size = cups.max)
+    @size = size
+    @next = Array.new(@size + 1)
+    cups.each_cons(2) do |a, b|
+      @next[a] = b
     end
-  
-    def delete(n)
-      return nil if n <= 0
-      last = self
-      n.times { last = last.next }
-      node = @next
-      @next = last.next
-      last.next = nil
-      node
+    last = cups.last
+    for cup in (cups.max + 1) .. @size
+      @next[last] = cup
+      last = cup
     end
-  
-    def insert(node)
-      return if node == nil
-      last = node
-      last = last.next until last.next == nil
-      last.next = @next
-      @next = node
-    end
-
-    def each
-      node = self
-      until node == nil
-        yield node.cup
-        node = node.next
-      end
-    end
-  end
-
-  def initialize(cups, size = cups.size)
-    @nodes = {}
-    head = Node.new(cups[0])
-    @nodes[head.cup] = head
-    tail = head
-    for cup in cups[1 ... size]
-      node = Node.new(cup)
-      @nodes[node.cup] = node
-      tail.next = node
-      tail = node
-    end
-    @min_cup = cups.min
-    @max_cup = cups.max
-    (size - cups.size).times do
-      @max_cup += 1
-      node = Node.new(@max_cup)
-      @nodes[node.cup] = node
-      tail.next = node
-      tail = node
-    end
-    tail.next = head
-    @curr = head
+    @next[last] = cups.first
+    @curr = cups.first
   end
 
   def mix(verbose = false)
-    picked_up = @curr.delete(3)
-    puts "pick up: #{picked_up.to_a.join(', ')}" if verbose
+    picked_up = self.next(@curr, 3)
+    puts "pick up: #{picked_up.join(', ')}" if verbose
 
     dest = @curr
     begin
-      dest = node(dest.cup - 1)
-    end while picked_up.include?(dest.cup)
-    puts "destination: #{dest.cup}" if verbose
+      dest = dest == 1 ? @size : dest - 1
+    end while picked_up.include?(dest)
+    puts "destination: #{dest}" if verbose
 
-    dest.insert(picked_up)
-    @curr = @curr.next
+    # Delete after current
+    @next[@curr] = @next[picked_up.last]
+
+    # Insert after destination
+    @next[picked_up.last] = @next[dest]
+    @next[dest] = picked_up.first
+
+    @curr = @next[@curr]
   end
 
-  def node(cup)
-    @nodes[wrap(cup)]
-  end
-
-  def wrap(cup)
-    (cup - @min_cup) % size + @min_cup
-  end
-
-  def size
-    @max_cup - @min_cup + 1
+  def next(cup, size)
+    Array.new(size) { cup = @next[cup] }
   end
 
   def to_s(offset = 0)
-    ["(#{@curr.cup})", *@curr.next.take(size - 1)].rotate(-offset).join(' ')
+    ["(#{@curr})", *self.next(@curr, size - 1)].rotate(-offset).join(' ')
   end
 end
-
-=begin
-cups = get_input(23).chomp.chars.map(&:to_i)
-min_cup = cups.min
-max_cup = cups.max
-(1_000_000 - cups.size).times do
-  max_cup += 1
-  cups << max_cup
-end
-10_000_000.times do |i|
-  picked_up = cups.slice!(1, 3)
-  dest = cups[0]
-  begin
-    dest = dest == min_cup ? max_cup : dest - 1
-  end while picked_up.include?(dest)
-
-  cups[cups.index(dest) + 1, 0] = picked_up
-
-  cups.rotate!(1)
-end
-cups.rotate!(cups.index(1))
-puts cups[1] * cups[2]
-=end
 
 =begin
 cups = Cups.new('389125467'.chars.map(&:to_i))
@@ -133,8 +63,8 @@ input = get_input(23).chomp.chars.map(&:to_i)
 
 cups = Cups.new(input)
 100.times { cups.mix }
-puts cups.node(1).next.take(cups.size - 1).join
+puts cups.next(1, cups.size - 1).join
 
 cups = Cups.new(input, 1_000_000)
 10_000_000.times { cups.mix }
-puts cups.node(1).next.take(2).prod
+puts cups.next(1, 2).prod
